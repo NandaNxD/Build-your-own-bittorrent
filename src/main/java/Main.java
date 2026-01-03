52c4c78233aa484f3744a65ef299022fb4174282
Nandan T S <nanven9@gmail.com>
2026-01-03 12:03:59 +0530

    Print trackerUrl and length:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
 
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
+
 import com.dampcake.bencode.Bencode;
+
+import static java.lang.System.out;
 
 public class Main {
     private static final Gson gson = new Gson();
@@ -19,13 +25,32 @@ public class Main {
                 Type type = bencode.type(bencodedValue.getBytes());
                 decoded = bencode.decode(bencodedValue.getBytes(), type);
             } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
+                out.println("Error in decoding args[1]");
+                out.println(e.getMessage());
                 return;
             }
-            System.out.println(gson.toJson(decoded));
+            out.println(gson.toJson(decoded));
+        }
+        else if("info".equals(command)){
+            String filePath=args[1];
+            try{
+                FileInputStream fileInputStream=new FileInputStream(filePath);
+                Bencode bencode=new Bencode();
+                Map<String,Object> torrentFileMap= bencode.decode(fileInputStream.readAllBytes(), Type.DICTIONARY);
+                String trackerURL=(String)torrentFileMap.get("announce");
+                Long length=(Long)((Map<String, Object>)torrentFileMap.get("info")).get("length");
 
-        } else {
-            System.out.println("Unknown command: " + command);
+                out.println("Tracker URL: "+trackerURL);
+                out.println("Length: "+length);
+            }
+            catch (Exception e){
+                out.println("Error in reading torrent file");
+                out.println(e.getMessage());
+                return;
+            }
+        }
+        else {
+            out.println("Unknown command: " + command);
         }
 
     }
