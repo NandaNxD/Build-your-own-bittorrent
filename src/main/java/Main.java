2835c58ed6242ae04cb08e96a3d642010118cdde
Nandan T S <nanven9@gmail.com>
2026-01-03 10:55:39 +0530

    Added integer support in bencode decode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,42 +2,45 @@ import com.google.gson.Gson;
 // import com.dampcake.bencode.Bencode;
 
 public class Main {
-  private static final Gson gson = new Gson();
+    private static final Gson gson = new Gson();
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    String command = args[0];
-    if("decode".equals(command)) {
-        String bencodedValue = args[1];
-        String decoded;
-        try {
-          decoded = decodeBencode(bencodedValue);
-        } catch(RuntimeException e) {
-          System.out.println(e.getMessage());
-          return;
+        String command = args[0];
+        if ("decode".equals(command)) {
+            String bencodedValue = args[1];
+            String decoded;
+            try {
+                decoded = decodeBencode(bencodedValue);
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+            System.out.println(gson.toJson(decoded));
+
+        } else {
+            System.out.println("Unknown command: " + command);
         }
-        System.out.println(gson.toJson(decoded));
 
-    } else {
-      System.out.println("Unknown command: " + command);
     }
 
-  }
-
-  static String decodeBencode(String bencodedString) {
-    if (Character.isDigit(bencodedString.charAt(0))) {
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i;
-          break;
+    static String decodeBencode(String bencodedString) {
+        if (Character.isDigit(bencodedString.charAt(0))) {
+            int firstColonIndex = 0;
+            for (int i = 0; i < bencodedString.length(); i++) {
+                if (bencodedString.charAt(i) == ':') {
+                    firstColonIndex = i;
+                    break;
+                }
+            }
+            int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
+            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
+        } else if (bencodedString.charAt(0) == 'i') {
+            int integerEndingIndex=bencodedString.indexOf('e');
+            return bencodedString.substring(1,integerEndingIndex);
+        } else {
+            throw new RuntimeException("Only strings are supported at the moment");
         }
-      }
-      int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-      return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-    } else {
-      throw new RuntimeException("Only strings are supported at the moment");
     }
-  }
-  
+
 }
